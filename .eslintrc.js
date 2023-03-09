b8ca97fc61d1059c15c218beb0446f1dad4944ff
gianfaye <git@gianfaye.com>
2023-03-09 18:49:52 +0800

    fix: lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,9 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: [
-    'react-app',
-    'airbnb-base',
-    'plugin:react/recommended',
-    'prettier'
-  ],
-  plugins: ['react', 'prettier'],
+  extends: ["react-app", "airbnb-base", "plugin:react/recommended", "prettier"],
+  plugins: ["react", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
   },
-}
+};
