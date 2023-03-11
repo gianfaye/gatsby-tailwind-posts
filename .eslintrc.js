7b4beade0c0b26f2cf3f429072322634e7d6a8c1
gianfaye <git@gianfaye.com>
2023-03-11 11:01:25 +0800

    chore: replace airbnb-base with airbnb-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: ["react-app", "airbnb-base", "plugin:react/recommended", "prettier"],
+  extends: ["react-app", "airbnb-typescript", "plugin:react/recommended", "prettier"],
   plugins: ["react", "prettier"],
   rules: {
     "prettier/prettier": "error",
+  },
+  parserOptions: {
+    project: ["./tsconfig.json"]
   },
 };
